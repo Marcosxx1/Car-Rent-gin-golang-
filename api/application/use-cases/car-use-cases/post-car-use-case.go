96bf67f0b68c9aae5cb35ed8cef7e4b8c821176e
Marcos Alexandre <marcosxx1.ma@gmail.com>
2024-02-16 08:12:49 -0400

    fix: Now will properly validate and don't allow db persistency if there is errors

--- api/application/use-cases/car-use-cases/post-car-use-case.go
+++ api/application/use-cases/car-use-cases/post-car-use-case.go
@@ -28,114 +28,93 @@ func NewPostCarUseCase(
 }
 
 func (useCase *PostCarUseCase) ExecuteConcurrently(inputDTO *dtos.CarInputDTO) (*dtos.CarOutputDTO, error) {
-	// Generate a new car ID
 	carID := xid.New().String()
 
-	// Create channels for results and errors
 	resultChan := make(chan *dtos.CarOutputDTO)
 	errorChan := make(chan error)
-	doneChan := make(chan struct{}) // Channel to signal completion or error
+	validationErrorSignal := make(chan bool) // Um sinal para caso tenhamos um erro > true 
 
-	// Create a waitgroup to wait for the completion of goroutines
 	var wg sync.WaitGroup
 
-	// Create a Once to ensure that channel closing occurs only once
-	var once sync.Once
-	closeChannels := func() {
-		once.Do(func() {
+	wg.Add(2)																		// passamos ele nas goroutines
+	go useCase.performValidation(&wg, errorChan, validationErrorSignal, inputDTO)
+	go useCase.performCarCreation(&wg, resultChan, errorChan, validationErrorSignal, carID, inputDTO)
+	
+	wg.Add(1)
+	go func() {
+			defer wg.Done()
+			wg.Wait()
 			close(resultChan)
 			close(errorChan)
-			close(doneChan)
-		})
-	}
+	}() 
 
-	// Create goroutines for each task
-	wg.Add(2)
-	go useCase.performValidation(&wg, errorChan, doneChan, inputDTO)
-	go useCase.performCarCreation(&wg, resultChan, errorChan, carID, inputDTO)
-
-	// Iniciamos a goroutine para fechar os canais e aguardar todas as tarefas terminarem
-	wg.Add(1) // adicionamos uma goroutine que aguarda e garante as conclusões das duas goroutines acima antes de chamar o closeChannels
-	go func() {
-		defer wg.Done() // executado por último
-		wg.Wait()       // Aguarda a conclusão das goroutines de validação e criação
-		closeChannels() // fecha!
-	}()
-
-	// Wait for results or errors
 	select {
-	case <-doneChan: // Check if an error has occurred
-		return nil, errors.New("validation or creation error")
 	case createdCar := <-resultChan:
-		return createdCar, nil
+			return createdCar, nil
 	case err := <-errorChan:
-		return nil, err
+			return nil, err
 	}
 }
 
-
-func (useCase *PostCarUseCase) performValidation(wg *sync.WaitGroup, errorChan chan<- error, inputDTO *dtos.CarInputDTO) {
+func (useCase *PostCarUseCase) performValidation(wg *sync.WaitGroup, errorChan chan<- error, validationErrorSignal chan<- bool, inputDTO *dtos.CarInputDTO) {
 	defer wg.Done()
 
-	// Validate the input structure
 	if err := validation_errors.ValidateStruct(inputDTO); err != nil {
-		errorChan <- err
-		return
+			errorChan <- err
+			validationErrorSignal <- true // Sinaliza que ocorreu um erro de validação
+			return
 	}
 
-	// Check if a car with the same license plate already exists
 	existCar, _ := useCase.carRepository.FindCarByLicensePlate(inputDTO.LicensePlate)
-	/* 	if err != nil {
-		errorChan <- err
-		return
-	} */
+
 	if existCar != nil {
-		// A car with the same license plate already exists
-		errorChan <- errors.New("a car with the same license plate already exists")
-		return
+			errorChan <- errors.New("a car with the same license plate already exists")
+			validationErrorSignal <- true // Sinaliza que ocorreu um erro de validação
+			return
 	}
+
+	validationErrorSignal <- false // Sinaliza que a validação foi bem-sucedida
 }
 
-func (useCase *PostCarUseCase) performCarCreation(wg *sync.WaitGroup, resultChan chan<- *dtos.CarOutputDTO, errorChan chan<- error, carID string, inputDTO *dtos.CarInputDTO) {
+func (useCase *PostCarUseCase) performCarCreation(wg *sync.WaitGroup, resultChan chan<- *dtos.CarOutputDTO, errorChan chan<- error, validationErrorSignal <-chan bool, carID string, inputDTO *dtos.CarInputDTO) {
 	defer wg.Done()
 
-	// Convert specification input to domain
+	if <-validationErrorSignal { // Verifica se houve erro de validação
+			// Não prossegue com a criação da instância se houver erro de validação
+			return
+	}
+
 	specifications := repoutils.ConvertSpecificationToDomainCreate(inputDTO.Specification, carID)
 
-	// Create a new car instance
 	newCar := &domain.Car{
-		ID:           carID,
-		Name:         inputDTO.Name,
-		Description:  inputDTO.Description,
-		DailyRate:    inputDTO.DailyRate,
-		Available:    inputDTO.Available,
-		LicensePlate: inputDTO.LicensePlate,
-		FineAmount:   inputDTO.FineAmount,
-		Brand:        inputDTO.Brand,
-		CategoryID:   inputDTO.CategoryID,
+			ID:           carID,
+			Name:         inputDTO.Name,
+			Description:  inputDTO.Description,
+			DailyRate:    inputDTO.DailyRate,
+			Available:    inputDTO.Available,
+			LicensePlate: inputDTO.LicensePlate,
+			FineAmount:   inputDTO.FineAmount,
+			Brand:        inputDTO.Brand,
+			CategoryID:   inputDTO.CategoryID,
 	}
 
-	// Register the new car
 	if err := useCase.carRepository.RegisterCar(newCar); err != nil {
-		errorChan <- fmt.Errorf("failed to create car record: %w", err)
-		return
+			errorChan <- fmt.Errorf("failed to create car record: %w", err)
+			return
 	}
 
-	// Post multiple specifications
 	if err := useCase.specificationRepository.PostMultipleSpecifications(specifications); err != nil {
-		errorChan <- fmt.Errorf("failed to create specification record: %w", err)
-		return
+			errorChan <- fmt.Errorf("failed to create specification record: %w", err)
+			return
 	}
 
 	resultChan <- convertToOutputDTO(carID, inputDTO)
-
 }
 
+
 func convertToOutputDTO(carID string, inputDTO *dtos.CarInputDTO) *dtos.CarOutputDTO {
-	// Assuming you have a function to convert specifications from inputDTO to outputDTO
 	specificaDomain := repoutils.ConvertSpecificationToDomainCreate(inputDTO.Specification, carID)
 	specificationOutPut := repoutils.ConvertSpecificationToDTO(specificaDomain)
-	// Create and return the output DTO
 	return &dtos.CarOutputDTO{
 		ID:            carID,
 		Name:          inputDTO.Name,

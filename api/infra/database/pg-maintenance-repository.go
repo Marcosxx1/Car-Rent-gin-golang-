55b04427954befff850641c5e8cedda654c14d45
Marcos Alexandre <marcosxx1.ma@gmail.com>
2024-02-22 18:07:44 -0400

    feat: Implement patch-maintenance endpoint

--- api/infra/database/pg-maintenance-repository.go
+++ api/infra/database/pg-maintenance-repository.go
@@ -1,8 +1,12 @@
 package database
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/Marcosxx1/Car-Rent-gin-golang-/api/domain"
 	dbconfig "github.com/Marcosxx1/Car-Rent-gin-golang-/api/infra/database/postgres/db-config"
+	"gorm.io/gorm"
 )
 
 type PGMaintenanceRepository struct{}
@@ -10,3 +14,32 @@ type PGMaintenanceRepository struct{}
 func (r *PGMaintenanceRepository) CreateMaintenance(maintenance *domain.Maintenance) error {
 	return dbconfig.Postgres.Create(maintenance).Error
 }
+
+func (r *PGMaintenanceRepository) GetMaintenanceByID(id string) (*domain.Maintenance, error){
+	var maintenance domain.Maintenance
+
+	err := dbconfig.Postgres.Where("id = ?", id, true).First(&maintenance).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, err
+		}
+		return nil, nil
+	}
+
+	return &maintenance, nil
+}
+
+func (r *PGMaintenanceRepository) UpdateMaintenance(maintenance *domain.Maintenance, id string) error {
+	result := dbconfig.Postgres.Model(&domain.Maintenance{}).Where("id = ?", id).Updates(maintenance)
+
+	if result.Error != nil {
+			return fmt.Errorf("failed to update maintenance record: %w", result.Error)
+	}
+
+	if result.RowsAffected == 0 {
+			return fmt.Errorf("no maintenance record found with id: %s", id)
+	}
+
+	return nil
+}
+

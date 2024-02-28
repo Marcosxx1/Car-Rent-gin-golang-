33ab838204eface163c47817c64faa861de36424
marcos <marcosxx1.ma@gmail.com>
2024-02-28 18:01:06 -0400

    feat: implements repository

--- api/infra/database/pg-maintenance-repository.go
+++ api/infra/database/pg-maintenance-repository.go
@@ -11,10 +11,22 @@ import (
 
 type PGMaintenanceRepository struct{}
 
+// /**
+//   - Creates a new maintenance record in the database.
+//     *
+//   - @param maintenance The maintenance record to create.
+//   - @return error If any error occurs while creating the maintenance record.
+//     */
 func (r *PGMaintenanceRepository) CreateMaintenance(maintenance *domain.Maintenance) error {
 	return dbconfig.Postgres.Create(maintenance).Error
 }
 
+// /**
+//   - Retrieves a single maintenance record by its ID.
+//     *
+//   - @param id The ID of the maintenance record to retrieve.
+//   - @return (*domain.Maintenance, error) A pointer to the retrieved maintenance record and any error encountered.
+//     */
 func (r *PGMaintenanceRepository) GetMaintenanceByID(id string) (*domain.Maintenance, error) {
 	var maintenance domain.Maintenance
 
@@ -29,6 +41,13 @@ func (r *PGMaintenanceRepository) GetMaintenanceByID(id string) (*domain.Mainten
 	return &maintenance, nil
 }
 
+// /**
+//   - Updates an existing maintenance record based on ID and provided information.
+//     *
+//   - @param maintenance The maintenance record with updated information.
+//   - @param id The ID of the maintenance record to update.
+//   - @return error If any error occurs while updating the maintenance record.
+//     */
 func (r *PGMaintenanceRepository) UpdateMaintenance(maintenance *domain.Maintenance, id string) error {
 	result := dbconfig.Postgres.Model(&domain.Maintenance{}).Where("id = ?", id).Updates(maintenance)
 
@@ -43,6 +62,12 @@ func (r *PGMaintenanceRepository) UpdateMaintenance(maintenance *domain.Maintena
 	return nil
 }
 
+// /**
+//   - Deletes a maintenance record based on ID.
+//     *
+//   - @param id The ID of the maintenance record to delete.
+//   - @return error If any error occurs while deleting the maintenance record.
+//     */
 func (r *PGMaintenanceRepository) DeleteMaintenance(id string) error {
 	result := dbconfig.Postgres.Where("id = ?", id).Delete(&domain.Maintenance{})
 
@@ -55,18 +80,22 @@ func (r *PGMaintenanceRepository) DeleteMaintenance(id string) error {
 	}
 
 	return nil
-
 }
 
+// /**
+//   - Retrieves all maintenance records paginated by provided page and page size.
+//   - Eagerly loads associated parts for each record.
+//     *
+//   - @param page The page number (starting from 1).
+//   - @param pageSize The number of records per page.
+//   - @return ([]*domain.Maintenance, error) A slice of pointers to retrieved maintenance records and any error encountered.
+//     */
 func (r *PGMaintenanceRepository) ListAllMaintenances(page, pageSize int) ([]*domain.Maintenance, error) {
 	var maintenances []*domain.Maintenance
 
-	// TODO retirar quando sair de dev
-	db := dbconfig.Postgres.Debug()
-
 	offset := (page - 1) * pageSize
 
-	err := db.Offset(offset).Limit(pageSize).Preload("Parts").Find(&maintenances).Error // 69
+	err := dbconfig.Postgres.Offset(offset).Limit(pageSize).Preload("Parts").Find(&maintenances).Error
 	if err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			return []*domain.Maintenance{}, nil
@@ -77,13 +106,19 @@ func (r *PGMaintenanceRepository) ListAllMaintenances(page, pageSize int) ([]*do
 	return maintenances, nil
 }
 
+//   - Retrieves all maintenance records associated with a specific car ID.
+//   - Eagerly loads associated parts for each record.
+//     *
+//
+// Parameters:
+//   - carID The ID of the car to retrieve associated maintenances for.
+//
+// Returns:
+//   - ([]*domain.Maintenance, error) A slice of pointers to retrieved maintenance records and any error encountered.
 func (r *PGMaintenanceRepository) GetMaintenancesByCarID(carID string) ([]*domain.Maintenance, error) {
 	var maintenances []*domain.Maintenance
 
-	// TODO retirar quando sair de dev
-	db := dbconfig.Postgres.Debug()
-
-	err := db.Where("car_id = ?", carID).Preload("Parts").Find(&maintenances).Error
+	err := dbconfig.Postgres.Where("car_id = ?", carID).Preload("Parts").Find(&maintenances).Error
 	if err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			return []*domain.Maintenance{}, nil
@@ -94,6 +129,15 @@ func (r *PGMaintenanceRepository) GetMaintenancesByCarID(carID string) ([]*domai
 	return maintenances, nil
 }
 
+// GetScheduledMaintenances retrieves an slice of maintenances
+//
+// Example:
+//
+//	maintenances := GetScheduledMaintenances()
+//
+// Returns
+//   - []*domain.Maintenance
+//   - error
 func (r *PGMaintenanceRepository) GetScheduledMaintenances() ([]*domain.Maintenance, error) {
 	var scheduledMaintenances []*domain.Maintenance
 
@@ -106,4 +150,24 @@ func (r *PGMaintenanceRepository) GetScheduledMaintenances() ([]*domain.Maintena
 	}
 
 	return scheduledMaintenances, nil
+}
+
+// - Retrieves all maintenance records with a specific maintenance status.
+// - Eagerly loads associated parts for each record.
+//
+// - @param status The maintenance status to filter by.
+//
+// - @return ([]*domain.Maintenance, error) A slice of pointers to retrieved maintenance records and any error encountered.
+func (r *PGMaintenanceRepository) GetMaintenancesByStatus(status string) ([]*domain.Maintenance, error) {
+	var maintenances []*domain.Maintenance
+
+	err := dbconfig.Postgres.Where("maintenance_status = ?", status).Preload("Parts").Find(&maintenances).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return []*domain.Maintenance{}, nil
+		}
+		return nil, err
+	}
+
+	return maintenances, nil
 }
